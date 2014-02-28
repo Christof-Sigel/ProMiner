0d0c9a07e1c5e233931e06c01ce4fc55cb159f21
Christof Sigel <christof.sigel@internode.on.net>
2014-02-28 20:34:10 +1030

    Start adding maps.

---
+++ Map.js
@@ -0,0 +1,23 @@
+
+function Map(seed)
+{
+    this.Seed=seed;
+    this.Chunks=[];
+    this.TilePool=new TilePool();
+}
+
+Map.prototype.GenerateChunk(x,y)
+{
+    //input x,y is based on chunk number (i.e. they increase by 1, not by ChunkSize)
+    this.Chunks[x][y]=new Chunk(x,y);
+    
+}
+
+Map.prototype.GetChunk(x,y)
+{
+    if(!this.Chunks[x][y])
+    {
+	this.GenerateChunk(x,y);
+    }
+    return this.Chunks[x][y];
+}
