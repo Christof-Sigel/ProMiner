8c68b1118a1d2a1e179e4ac8e3b17eb4558e6da0
Christof Sigel <tnvgetstuff@gmail.com>
2015-12-15 00:40:21 +1030

    Merge changes.

--- InitGame.js
+++ InitGame.js
@@ -1,33 +1,35 @@
 'use strict';
 
-var Tiles=[]
-var Context;
+var Tiles = [];
+var Canvas;
 var Context3d;
 var NewImage;
 var FShader;
 var VShader;
 
+var GameState={x:0,y:0,width:2048,height:256};
+
 function init(){
-    var Canvas = document.getElementById("Canvas");
-   // Context = Canvas.getContext("2d");
+	Canvas = document.getElementById("Canvas");
+	// Context = Canvas.getContext("2d");
     Context3d = InitWebGL(Canvas);
     var seed = Math.random()*500;
     var PerlinNoise=OpenSimplex.create(seed);
 
-    for(var x=0;x<64;x++){
-	Tiles[x]=[];
-	for(var y=0;y<64;y++){
-	    var data = Math.floor((PerlinNoise.noise3D(x/16,y/16,0.2)+1)*127);
-
-	    Tiles[x][y] = data;//Context.drawImage(NewImage, x*32, y*32);
+   	for(var x=0;x<GameState.width;x++){
+		Tiles[x]=[];
+		for(var y=0;y<GameState.height;y++){
+			var data = Math.floor((PerlinNoise.noise2D(x/16,y/16)+1)*127);
+			if(data < 200){
+				Tiles[x][y]=1;
+			}
+		}
 	}
-    }
     FShader = LoadShader("test.fs.glsl");
     VShader = LoadShader("test.vs.glsl");
 	NewImage = LoadImage("Ground.png");
 	NewImage.onload=function(){
 	    setInterval(Render3d, 500);
-		//Context.drawImage(NewImage, 32, 32);
 	}
 }
 
@@ -37,9 +39,9 @@ function LoadImage(path){
     image.Loaded=false;
     image.Percent=0;
     image.onload=function(){
-	this.Loaded=true;
-	this.Percent=100;
-	//TODO(Christof): Update Loading UI here?
+		this.Loaded=true;
+		this.Percent=100;
+		//TODO(Christof): Update Loading UI here?
     }
     image.src="images/"+path;
     image.Name="Image: "+path;
@@ -47,19 +49,6 @@ function LoadImage(path){
     return image;
 }
 
-function Render(){
-    for(var x=0;x<64;x++){
-	for(var y=0;y<64;y++){
-	    var data = Tiles[x][y];
-	    if(data<100){
-		Context.drawImage(NewImage, x*32, y*32);
-	    }
-	    Context.fillStyle="rgb("+data+","+data+","+data+")";
-	    Context.fillRect(64*32+x*8,y*8,8,8);
-	}
-    }
-
-}
 
 function Render3d(){
     if(FShader.shader !== undefined && VShader.shader !== undefined){
@@ -71,17 +60,17 @@ function InitWebGL(canvas){
     var gl = null;
 
     try {
-	// Try to grab the standard context. If it fails, fallback to experimental.
-	gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+		// Try to grab the standard context. If it fails, fallback to experimental.
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
     }
     catch(e) {
-	console.log(e)
+		console.log(e)
     }
 
     // If we don't have a GL context, give up now
     if (!gl) {
-	alert("Unable to initialize WebGL. Your browser may not support it.");
-	gl = null;
+		alert("Unable to initialize WebGL. Your browser may not support it.");
+		gl = null;
     }
 
     return gl;
@@ -103,8 +92,8 @@ function CreateShader(Type,Source, gl){
 
     // See if it compiled successfully
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-	console.log("An error occurred compiling shader: " + gl.getShaderInfoLog(shader));
-	return null;
+		console.log("An error occurred compiling shader: " + gl.getShaderInfoLog(shader));
+		return null;
     }
 
     return shader;
@@ -127,16 +116,16 @@ function LoadShader(path){
             // If we got HTTP status 200 (OK)
             if (request.status == 200) {
                 script.loaded=true;
-		script.data=request.responseText;
-		var shader;
-		if(path.endsWith('.fs.glsl')){
-		    shader = CreateFragmentShader(script.data, Context3d);
-		}else if(path.endsWith('.vs.glsl')){
-		    shader = CreateVertexShader(script.data, Context3d);
-		}
-		script.shader=shader;
+				script.data=request.responseText;
+				var shader;
+				if(path.endsWith('.fs.glsl')){
+					shader = CreateFragmentShader(script.data, Context3d);
+				}else if(path.endsWith('.vs.glsl')){
+					shader = CreateVertexShader(script.data, Context3d);
+				}
+				script.shader=shader;
             } else { // Failed
-               console.log("Failed to load script"+path);
+				console.log("Failed to load script"+path);
             }
         }
     };
@@ -149,4 +138,17 @@ if (typeof String.prototype.endsWith !== 'function') {
     String.prototype.endsWith = function(suffix) {
         return this.indexOf(suffix, this.length - suffix.length) !== -1;
     };
+}
+
+function Render(){
+	Context.clearRect(0,0,64*32,64*32);
+	for(var x = 0;x<64;x++){
+		for(var y = 0;y<64;y++){
+			if(Tiles[GameState.x+x][GameState.y+y] !== undefined){
+				Context.drawImage(NewImage,x*32, y*32);
+			}
+		}
+	}
+	GameState.y+=8;
+	setTimeout(Render, 1000);
 }
